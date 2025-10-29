05738a8f531c6e318b70fabc1d5d51f7ca0c5055
RaviCoderWallah <raviverma05042023@gmail.com>
2025-10-29 17:29:13 +0530

    feat: bill input validation that if bill is not value then everthing is disabled and if value then enabled.

--- Bill-Splliter/src/components/BillSplitCalculate.jsx
+++ Bill-Splliter/src/components/BillSplitCalculate.jsx
@@ -1,19 +1,53 @@
+import { useState } from "react"
+
 const BillSplitCalculate = () => {
+
+  const [billAmount, setBillAmount] = useState(null);
+  const [tipAmount, setTipAmount] = useState(null);
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleBillAmountInput = (e) => {
+    if(e.target.value == ""){
+      setBillAmount(null);
+    }else{
+      setBillAmount(e.target.value);
+    }
+  }
+
+
+  const handleTipPercentage = (event, index) => {
+    if (billAmount && billAmount.length !== 0) {
+      if (activeIndex === index) {
+        setActiveIndex(null);
+      } else {
+        const amount = parseInt(event.target.innerText);
+        setTipAmount(amount);
+
+        setActiveIndex(index);
+
+        console.log(tipAmount);
+      }
+    }
+  }
+
+
   return (
     <div className="bill-split-calculate-container">
       <div className="bill-input">
-        <p className="label">Bill</p>
+        <p className="label">Bill {billAmount}</p>
         <div className="input-container">
           <span>₹</span>
-          <input type="number" />
+          <input type="number" onChange={handleBillAmountInput} />
         </div>
       </div>
       <div className="bill-tip">
         <p className="label">Select Tip</p>
-        <div className="select-tip-container">
+        <div className={(billAmount && billAmount.length !== 0) ? "select-tip-container active" : "select-tip-container"}>
           {
             [5, 10, 15, 25, 50, 75].map((tipPercentage, index) => {
-              return <div className="tip-percentage" key={index}>{tipPercentage}%</div>
+              return <div className={`tip-percentage ${(billAmount && activeIndex === index )? "true" : ""}`} onClick={(event) => handleTipPercentage(event, index)} key={index}>
+                <b>{tipPercentage}</b>%
+              </div>
             })
           }
         </div>
@@ -21,7 +55,7 @@ const BillSplitCalculate = () => {
       <div className="bill-split-number">
         <p className="label">Number Of People</p>
         <div>
-          <input type="number" placeholder="No of people" disabled/>
+          <input type="number" placeholder="No of people" disabled />
         </div>
       </div>
       <button className="generate-bill-btn">Generate Bill</button>
