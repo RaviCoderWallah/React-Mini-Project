920c80a9702b440af030173173ee50f295430f52
RaviCoderWallah <raviverma05042023@gmail.com>
2025-11-08 21:30:48 +0530

    feat: implement card shuffling and update match logic in Memory Card Game

--- 04_Memory-Card-Game/src/App.jsx
+++ 04_Memory-Card-Game/src/App.jsx
@@ -1,12 +1,62 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
 
+  const [clickedCard, setClickCard] = useState();
+  const [cards, setCards] = useState([]);
+  const [matchCards, setMatchCards] = useState([]);
+
+
+  const emojiList = ["🦜", "🍎", "🌻", "🚗", "🧭", "🐘"];
+
+  useEffect(() => {
+    //suffle card logic so every time cards position changes
+    const shuffle = (arr) => {
+      const copy = [...arr];
+      for (let i = copy.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [copy[i], copy[j]] = [copy[j], copy[i]];
+      }
+      return copy;
+    }
+
+    const doubleEmojiList = emojiList.concat(emojiList);
+    const shuffleCards = shuffle(doubleEmojiList);
+    setCards(shuffleCards);
+  }, [])
+
+
+
+  const handleClick = (index) => {
+    setClickCard(cards[index]);
+
+    if(matchCards.length === 0){
+       setMatchCards(clickedCard);
+    }else{
+      setMatchCards((prev) => [...prev, clickedCard])
+    }
+
+    console.log(matchCards);
+  }
+
+
   return (
     <>
-
+      <h1>Memory Card Game {clickedCard} </h1>
+      <div className='memory-card-game'>
+        {cards.map((image, index) => (
+          <div
+            className="memory card"
+            key={index}
+            onClick={() => handleClick(index)}
+          >
+            {image}
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
